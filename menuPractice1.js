67db6f0fff718610042478bf50a640ae3edd9f33
Ruben Murga <ruben.murgat@gmail.com>
2024-12-19 18:01:38 -0600

    updating code

--- menuPractice1.js
+++ menuPractice1.js
@@ -2,25 +2,88 @@ import readLineSync from 'readline-sync'
 import ListBundle01 from './solutions/ListBundle01.js';
 
 var doprintingSumTwo = function () {
-    const var1 = new ListBundle01();
+    const lib = new ListBundle01();
     var arr = [2,7,11,15];
     console.log("[Sum Two]");
-    console.log("Answer No. 1 [" + var1.twoSum(arr,9) + "]")
+    console.log("Answer No. 1 [" + lib.twoSum(arr,9) + "]")
 }
 
 var doprintingContainsDuplicate = function() {
-    const var1 = new ListBundle01();
+    const lib = new ListBundle01();
     var arr = [2,7,11,15,11];
     console.log("[ Contains Duplicate ]");
-    console.log("Answer No. 1: " + var1.containsDuplicate_1(arr))
-    console.log("Answer No. 2: " + var1.containsDuplicate_2(arr))
-    console.log("Answer No. 2: " + var1.containsDuplicate_3(arr))    
+    console.log("Answer No. 1: " + lib.containsDuplicate_1(arr))
+    console.log("Answer No. 2: " + lib.containsDuplicate_2(arr))
+    console.log("Answer No. 2: " + lib.containsDuplicate_3(arr))    
 }
 
 var doprintingValidAnagram = function() {
-    const var1 = new ListBundle01();
+    const lib = new ListBundle01();
     console.log("[ Valid Anagram ]");
-    console.log("Answer No. 1: " + var1.isAnagram("anagram", "nagaram"));
+    console.log("Answer No. 1: " + lib.isAnagram("anagram", "nagaram"));
+}
+var doprintingGroupAnagram = function() {
+    const lib = new ListBundle01();
+    console.log("[ Group Anagram ]");
+    console.log("Answer No.1: " + lib.groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
+}
+var doprintingReplaceElements = function() {
+    const lib1 = new ListBundle01();
+    console.log("[ Replace Elements ]");
+    console.log("Answer No.1: " + lib1.replaceElements([17,18,5,4,6,1]))
+}
+var doprintingIsSubsequence = function() {
+    const lib1 = new ListBundle01();
+    console.log("[ Is Subsquence  ]");
+    console.log("Answer No. 1:" + lib1.isSubsequence("abc", "ahbgdc"));
+}
+var doprintingLengthLastWord = function() {
+    const lib1 = new ListBundle01();
+    console.log("[ Length Last word  ]");
+    console.log("Answer No. 1: " + lib1.lengthOfLastWord("   fly me   to   the moon  "));
+    console.log("Answer No. 2: " + lib1.lengthOfLastWord("Hello World"));
+    console.log("Answer No. 3: " + lib1.lengthOfLastWord("luffy is still joyboy"));
+    console.log("Answer No. 4: " + lib1.lengthOfLastWord("luffy"));
+    console.log("Answer No. 5: " + lib1.lengthOfLastWord(""));    
+}
+var doprintingLongestCommonPrefix = function() {
+    const lib1 = new ListBundle01();
+    console.log("[ LongestCommonPrefix  ]");
+    console.log ("Answer No. 1, type=1: " + lib1.longestCommonPrefix_1(["flower","flow","flight"]) );
+    console.log ("Answer No. 2, type=1: " + lib1.longestCommonPrefix_1(["dog","racecar","car"]) );
+    console.log ("Answer No. 3, type=1: " + lib1.longestCommonPrefix_1(["car"]) );
+    console.log ("Answer No. 4, type=1: " + lib1.longestCommonPrefix_1([""]) );
+    console.log ("Answer No. 1, type=2: " + lib1.longestCommonPrefix_2(["flower","flow","flight"]));
+    console.log ("Answer No. 2, type=2: " + lib1.longestCommonPrefix_2(["dog","racecar","car"]));
+    console.log ("Answer No. 3, type=2: " + lib1.longestCommonPrefix_2(["car"]));
+    console.log ("Answer No. 4, type=2: " + lib1.longestCommonPrefix_2([""]));
+}
+var doprintingTopKFrequentElement = function() {
+    const lib1 = new ListBundle01();
+    console.log("[ Top K Frequent Element ]");
+    console.log ("Answer No. 1, type=1: " + lib1.topKFrequent([1,1,1,2,2,3],2) );
+    console.log ("Answer No. 1, type=1: " + lib1.topKFrequent([1],1) );
+}
+var doprintingEncodeDecodeString = function() {
+    const lib1 = new ListBundle01();
+    const array1 = ['neet','code','love','you'];
+    console.log(" [ Enconde / Decode String Array] ");
+    var encoded = lib1.encode(array1);
+    console.log("Encode Answer no. 1:" + encoded);
+    console.log("Decoded Answer no. 1:");
+    const ans = lib1.decode(encoded);
+    ans.forEach(s=> {
+        console.log(s);
+    })
+}
+var doprintingArrayExceptSelf = function() {
+    const lib1 = new ListBundle01();
+    const array1 = [1,2,3,4];
+    console.log(" [ Product Array Except Self ] ");
+    const array2 = lib1.productExceptSelf(array1);
+    array2.forEach(i=> {
+        console.log(i)
+    })
 }
 
 let userRes;
@@ -29,6 +92,14 @@ while (userRes !== '0') {
     console.log("1. Two Sum (LC#1)")
     console.log("2. Contains Duplicate (LC#217)")
     console.log("3. Valid Anagram (LC#242)")
+    console.log("4. Group Anagram (LC#49)")
+    console.log("5. Replace Elements (LC#1299)")
+    console.log("6. Is Subsequence (LC#392)")
+    console.log("7. Length of Last Word (LC#58)")
+    console.log("8. Longest Common Prefix (LC#14)")
+    console.log("9. Top K Frequent Element (LC#347)")
+    console.log("10. Encode and Decode String (neetcode)");
+    console.log("11. Product of Array Except Self (LC#238)");
     userRes = readLineSync.question("[pick an option:] ");
     switch(userRes) {
         case '1':
@@ -40,11 +111,29 @@ while (userRes !== '0') {
         case '3':
             doprintingValidAnagram();
             break;
-
+        case '4':
+            doprintingGroupAnagram();
+            break;
+        case '5':
+            doprintingReplaceElements();
+            break;
+        case '6':
+            doprintingIsSubsequence();
+            break;
+        case '7':
+            doprintingLengthLastWord();
+            break;
+        case '8':
+            doprintingLongestCommonPrefix();
+            break;
+        case '9':
+            doprintingTopKFrequentElement();
+            break;
+        case '10':
+            doprintingEncodeDecodeString();
+            break;
+        case '11':
+            doprintingArrayExceptSelf();
+            break;
     }
-/*    if (userRes === '1') {
-        doprintingSumTwo()
-    } else if (userRes === '2') {
-        doprintingContainsDuplicate()
-    } */
 }
--- solutions/ListBundle01.js
+++ solutions/ListBundle01.js
@@ -74,4 +74,142 @@ export default class ListBundle01 {
         return true;
     }
 
+    groupAnagrams = function(strs) {
+        let ht = {}
+        strs.forEach(s => {
+            let ss = s.split("").sort();
+            if (ht[ss]!=null){
+                let arr = ht[ss];
+                arr.push(s)
+                ht[ss]=arr;
+            } else
+                ht[ss]=[s];
+        });
+        return Object.values(ht);
+    }
+
+    replaceElements = function(arr) {
+        let ans = [];
+        let max1 = -1;
+        for (let i=arr.length-1; i>-1; i--) {
+            ans.push(max1);
+            max1 = Math.max(max1, arr[i]);
+        }
+        return ans.reverse();
+    }
+
+    isSubsequence = function(s, t) {
+        let L = s.length;
+        if(!L) return true;
+        let i = 0;
+        let j = 0;
+        let located = 0
+        while (j < t.length) {
+            if (s[i]==t[j]) {
+              i++;
+              located++;
+            }
+            j++;
+        }
+        if(located==L) return true;
+        return false;
+    };
+
+    lengthOfLastWord = function(s) {
+        let ns = s.trim();
+        let L = ns.length-1;
+        let j = L;
+        while (j>-1) {
+            if(ns[j]==' ') break;
+            j--;
+        }
+        return L-j;
+    }
+
+    longestCommonPrefix_1 = function (strs) {
+        let ans = "";
+        strs.sort();
+        let first = strs[0];
+        let last = strs[strs.length-1];
+        for(let i=0; i<Math.min(first.length,last.length); i++) {
+            if(first.charAt(i)!=last.charAt(i)) break;
+            ans+=first.charAt(i);
+        }
+        return ans;
+    }       
+    longestCommonPrefix_2 = function (strs) {    
+        if(strs==null || strs.length==0) return "";
+        let prefix = strs[0];
+        strs.forEach(s => {
+            while(s.indexOf(prefix)!=0) 
+                prefix = prefix.substring(0, prefix.length-1);
+        });
+        return prefix;        
+    }
+
+    topKFrequent = function(nums, k) {
+        var res = [];
+        const map1 = new Map();
+        nums.forEach(e => {
+            let located = map1.get(e);
+            if(located>-1) 
+                map1.set(e, located+1);
+            else
+                map1.set(e, 1);
+        });
+        const mapSort1 = new Map([...map1.entries()].sort((a, b) => b[1] - a[1]));
+        let i=0
+        const mapIterator = mapSort1.keys();
+        while (i < k) {
+            res.push(mapIterator.next().value);
+            i++;
+          }        
+        return res;
+    }
+
+    encode = function(strs) {
+        var ans = "";
+        strs.forEach(s => {
+            const achars = s.split("");
+            achars.forEach(c => {
+                var i = c.charCodeAt(0) << 2;
+                ans += String.fromCharCode(i);
+            });
+            ans += String.fromCharCode(10);
+        });
+        return ans;
+    }
+
+    decode = function(s) {
+        var ans = new Array();
+        const a = s.split("");
+        var ele = "";
+        a.forEach(c => {
+            if(c.charCodeAt(0)==10) {
+                ans.push(ele);
+                ele = "";
+            } else {
+                var i = c.charCodeAt(0) >> 2;
+                ele += String.fromCharCode(i);
+            }
+        });
+        return ans;
+    }
+
+    productExceptSelf = function(nums) {
+        const ans = new Array(nums.length);
+        var left = 1
+        for(let i=0; i<nums.length; i++) {
+            ans[i] = left;
+            left = left * nums[i];
+        }
+        var right = 1;
+        for(let i=nums.length-1; i>-1; i--) {
+            ans[i] = right * ans[i];
+            right = right * nums[i];
+        }
+        return ans;
+    }
+  
+
 }
